c258b753a4dd661de4b9ee79e63c91f7924d895d
rin <rinaldychou.k@gmail.com>
2025-12-11 09:29:54 +0800

    ex1_dictionary

---
+++ Dictionary/DIctionary1.py
@@ -0,0 +1,14 @@
+# Bsic directory
+dta_list = ["xin","sunho","linhao"]
+data_dict = {
+	"x":"value1",
+	"y":"value2",
+	"z":"value3",
+	"x1": 111,
+    "xin":dta_list,
+}
+print(data_dict)
+
+print(data_dict["x"])
+print(data_dict["x1"])
+print(data_dict["xin"])
