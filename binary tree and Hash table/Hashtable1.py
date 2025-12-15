1089e4f09715999f31d52d8ab2740bc60ab1596a
rin <rinaldychou.k@gmail.com>
2025-12-16 01:13:01 +0800

    ex1_Hash_table.py

--- binary tree and Hash table/Hashtable1.py
+++ binary tree and Hash table/Hashtable1.py
@@ -8,12 +8,11 @@ hash_table = [None] * panjang
 
 ind = Hashing("rin", panjang)
 hash_table[ind] = 100
-
+print(f"nilai rin adalah {hash_table[ind]}")
 ind = Hashing("Yanto", panjang)
 hash_table[ind] = 90
-
-print("\Hash Table setelaha 2 data masuk:")
+print(f"nilai yanto adalah {hash_table[ind]}")
+print("\n Hash Table setelah 2 data masuk:")
 print(hash_table)
 
-ind = Hashing("rin", panjang)
-print(hash_table[ind])
+
